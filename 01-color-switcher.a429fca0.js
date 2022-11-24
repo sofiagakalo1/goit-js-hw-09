cc236c6e84b714dd81705919793fcb4756f2de45
sofiagakalo1 <108238510+sofiagakalo1@users.noreply.github.com>
2022-11-24 16:28:56 +0000

    Deploying to gh-pages from @ sofiagakalo1/goit-js-hw-09@7d16e7ed1d47e4bfd67188a117b2c8fee57a7d44 🚀

---
+++ 01-color-switcher.a429fca0.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function n(){t.startBtn.disabled?(t.startBtn.disabled=!1,t.stopBtn.disabled=!0):(t.startBtn.disabled=!0,t.stopBtn.disabled=!1)}t.startBtn.addEventListener("click",(function(){n(),idInterval=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),t.stopBtn.addEventListener("click",(function(){n(),clearInterval(idInterval)}));
+//# sourceMappingURL=01-color-switcher.a429fca0.js.map
